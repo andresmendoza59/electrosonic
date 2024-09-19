0e4d7f59c50e50a40c567174c2adf59a8540b0ff
Andrés Mendoza <140727099+andresmendoza59@users.noreply.github.com>
2024-09-19 09:23:07 -0500

    Update plot_waveform.py
    Added style 538-like if it is available, otherwise, use default matplotlib's plotting style.

--- src/plot_waveform.py
+++ src/plot_waveform.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import FuncFormatter
 
 from waveio import WAVReader
 
@@ -17,6 +19,11 @@ def parse_args():
     return parser.parse_args()
 
 def plot(filename, metadata, channels):
+    try:
+        plt.style.use("fivethirtyeight")
+    except OSError:
+        pass
+    
     fig, ax = plt.subplots(
         nrows=metadata.num_channels,
         ncols=1,
@@ -27,15 +34,29 @@ def plot(filename, metadata, channels):
     if isinstance(ax, plt.Axes):
         ax = [ax]
 
+    time_formatter = FuncFormatter(format_time)
+    timeline = np.linspace(
+        start=0,
+        stop=metadata.num_seconds,
+        num=metadata.num_frames
+    ) 
+
     for i, channel in enumerate(channels):
         ax[i].set_title(f"Channel #{i + 1}")
         ax[i].set_yticks([-1, 0.5, 0, 0.5, 1])
-        ax[i].plot(channel)
+        ax[i].xaxis.set_major_formatter(time_formatter)
+        ax[i].plot(timeline, channel)
 
     fig.canvas.manager.set_window_title(filename)
     plt.tight_layout()
     plt.show()
 
+def format_time(instant, _):
+    if instant < 60:
+        return f"{instant:g}s"
+    minutes, seconds = divmod(instant, 60)
+    return f"{minutes:g}m {seconds:02g}s"
+
 if __name__ == "__main__":
     main()
-  
+    
